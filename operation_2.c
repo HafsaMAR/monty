645d9dae735f9f7ec270891fbbaeeda8fa2d6da7
HafsaMAR <marouche.hafsa@gmail.com>
2023-08-24 22:29:31 +0100

    sub_operation

--- operation_2.c
+++ operation_2.c
@@ -39,3 +39,27 @@ void add(stack_t **stack, unsigned int line_number)
 	(*stack)->n = sum;
 	free(temp);
 }
+
+void nop(stack_t **stack, uint line_number)
+{
+	(void)stack;
+	(void)line_number;
+}
+
+void sub(stack_t **stack, uint line_number)
+{
+	stack_t *temp;
+	int difference;
+
+	if (!*stack || !(*stack)->next)
+	{
+		fprintf(stderr, "L%d: can't sub, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	diff = (*stack)->next->n - (*stack)->n;
+	temp = *stack;
+	*stack = (*stack)->next;
+	(*stack)->n = diff;
+	free(temp);
+}
