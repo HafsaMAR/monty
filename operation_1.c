5613b486943cdfe1f6baecfe1d44754986386272
HafsaMAR <marouche.hafsa@gmail.com>
2023-08-24 21:28:28 +0100

    pint_operation

--- operation_1.c
+++ operation_1.c
@@ -55,4 +55,15 @@ void pall(stack_t **stack, unsigned int line_number)
 	}
 }
 
+void pint(stack_t **stack, unsigned int line_number)
+{
+
+	if ((*stack) == NULL)
+	{
+		printf("L%d: can't pint, stack empty\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	printf("%d\n", (*stack)->n);
+}
+
 
