be755d8cc153f899bf94b981ceff50bd6c1f39ab
HafsaMAR <marouche.hafsa@gmail.com>
2023-08-24 22:25:59 +0100

    add_operation

--- operation_2.c
+++ operation_2.c
@@ -22,3 +22,20 @@ void swap(stack_t **stack, unsigned int line_number)
 	element2->prev = NULL;
 	*stack = element2;
 }
+
+void add(stack_t **stack, unsigned int line_number)
+{
+	stack_t *temp;
+	int sum;
+
+	if (*stack == NULL || !(*stack)->next == NULL)
+	{
+		fprintf(stderr, "L%d: can't add, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	sum = (*stack)->n + (*stack)->next->n;
+	temp = *stack;
+	stack = (*stack)->next;
+	(*stack)->n = sum;
+	free(temp);
+}
